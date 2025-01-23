396b22d96b77598ac2ca82745b30c97cee3af5b8
FRANKD-UX <frankdafrica@gmail.com>
2025-01-22 22:29:37 -0800

    taks1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# This manifest installs Flask version 2.1.0 using pip3.
+# This manifest installs Flask version 2.1.0 and ensures compatibility with Werkzeug.
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
