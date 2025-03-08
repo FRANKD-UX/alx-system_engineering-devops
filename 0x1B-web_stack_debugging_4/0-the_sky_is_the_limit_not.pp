baf4330f3826f9bebd7df4e76eb17a40f34892d5
FRANKD-UX <frankdafrica@gmail.com>
2025-03-08 14:30:15 -0800

    1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,28 +1,6 @@
-# This Puppet manifest fixes Nginx configuration to handle high traffic loads
-# by increasing worker connections, adjusting buffer sizes, and raising system limits.
-
-# Increase Nginx worker connections
-exec { 'increase_nginx_worker_connections':
-  command => 'sed -i "s/worker_connections.*/worker_connections 4096;/" /etc/nginx/nginx.conf',
-  path    => '/bin:/usr/bin',
-  unless  => 'grep "worker_connections 4096;" /etc/nginx/nginx.conf',
-}
-
-# Increase system file descriptor limit
-exec { 'increase_file_descriptor_limit':
-  command => 'ulimit -n 65536',
-  path    => '/bin:/usr/bin',
-}
-
-# Reload Nginx to apply changes
-exec { 'reload_nginx':
-  command => 'service nginx reload',
-  path    => '/bin:/usr/bin',
-  require => Exec['increase_nginx_worker_connections'],
-}
-
-# Ensure Nginx is running
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
+# Fixes Nginx configuration to handle high traffic by increasing worker connections and file descriptors
+exec { 'fix--for-nginx':
+  command => '/bin/sed -i "s/worker_connections [0-9]*;/worker_connections 4096;/g; s/# ULIMIT=/ULIMIT=/g; s/ULIMIT=.*/ULIMIT=\"-n 8192\"/g" /etc/nginx/nginx.conf /etc/default/nginx && sudo service nginx restart',
+  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  onlyif  => 'test -f /etc/nginx/nginx.conf',
 }
