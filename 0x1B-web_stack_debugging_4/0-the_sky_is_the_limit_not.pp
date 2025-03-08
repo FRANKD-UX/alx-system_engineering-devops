f7bef3ff8e757f7cf46421c1ce13efa5bf1fa518
FRANKD-UX <frankdafrica@gmail.com>
2025-03-08 14:59:08 -0800

    1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,22 @@
 # Fixes Nginx configuration to handle high traffic by increasing worker connections and file descriptors
-exec { 'fix--for-nginx':
-  command => '/bin/sed -i "s/worker_connections [0-9]*;/worker_connections 4096;/g; s/# ULIMIT=/ULIMIT=/g; s/ULIMIT=.*/ULIMIT=\"-n 8192\"/g" /etc/nginx/nginx.conf /etc/default/nginx && sudo service nginx restart',
+
+exec { 'increase_worker_connections':
+  command => '/bin/sed -i "s/worker_connections [0-9]*;/worker_connections 4096;/g" /etc/nginx/nginx.conf',
   path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
   onlyif  => 'test -f /etc/nginx/nginx.conf',
-}# Fixes Nginx configuration to handle high traffic by increasing worker connections and file descriptors
-exec { 'fix--for-nginx':
-  command => '/bin/sed -i "s/worker_connections [0-9]*;/worker_connections 4096;/g; s/# ULIMIT=/ULIMIT=/g; s/ULIMIT=.*/ULIMIT=\"-n 8192\"/g" /etc/nginx/nginx.conf /etc/default/nginx && sudo service nginx restart',
-  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-  onlyif  => 'test -f /etc/nginx/nginx.conf',
+  notify  => Service['nginx'],
 }
+
+exec { 'increase_ulimit':
+  command => '/bin/sed -i "s/^# ULIMIT=.*$/ULIMIT=\"-n 65535\"/" /etc/default/nginx',
+  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  onlyif  => 'test -f /etc/default/nginx',
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+}
+
