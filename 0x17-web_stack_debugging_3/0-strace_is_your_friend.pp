47a08f4ff29121c780590e2cf62429984e65c827
FRANKD-UX <frankdafrica@gmail.com>
2025-03-09 16:38:18 -0700

    task0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,15 @@
-# 0-strace_is_your_friend.pp
-# This Puppet manifest fixes the WordPress web stack by updating the DB host in
-# wp-config.php from 'localhost' (which uses the Unix socket and causes a permission denied error)
+# This manifest fixes the WordPress web stack by updating the DB host in 
+# wp-config.php from 'localhost' (using the Unix socket with permission issues)
 # to '127.0.0.1' (forcing a TCP connection). It then restarts Apache.
 #
-# Requirements:
-#   - WordPress is installed in /var/www/html
-#   - Puppet manifest passes puppet-lint version 2.1.1
-#   - Apache runs on Ubuntu 14.04 LTS (checked with Puppet v3.4)
+# Designed for Ubuntu 14.04 LTS and Puppet v3.4.
+#
+# The manifest passes puppet-lint v2.1.1.
 
-file_line { 'fix-wp-config-db-host':
-  path  => '/var/www/html/wp-config.php',
-  match => '^define\(\'DB_HOST\',',
-  line  => 'define(\'DB_HOST\', \'127.0.0.1\');',
+file_line { 'wp-config_db_host':
+  path   => '/var/www/html/wp-config.php',
+  match  => '^define\\(\'DB_HOST\',',
+  line   => 'define(\'DB_HOST\', \'127.0.0.1\');',
   notify => Service['apache2'],
 }
 
