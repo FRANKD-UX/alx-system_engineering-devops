4df6d421d38768b06aee86ab7ceda2fd877f06d3
FRANKD-UX <frankdafrica@gmail.com>
2025-03-09 16:44:38 -0700

    task0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,20 +1,16 @@
-# This manifest fixes the WordPress web stack by updating the DB host in 
-# wp-config.php from 'localhost' (using the Unix socket with permission issues)
-# to '127.0.0.1' (forcing a TCP connection). It then restarts Apache.
+# 0-strace_is_your_friend.pp
+# This manifest fixes the WordPress LAMP stack by replacing the DB host in
+# wp-config.php. It changes 'localhost' (which causes a Unix socket permission
+# error) to '127.0.0.1' (forcing a TCP connection). It then restarts Apache.
 #
 # Designed for Ubuntu 14.04 LTS and Puppet v3.4.
-#
-# The manifest passes puppet-lint v2.1.1.
+# This manifest passes puppet-lint v2.1.1.
 
-file_line { 'wp-config_db_host':
-  path   => '/var/www/html/wp-config.php',
-  match  => '^define\\(\'DB_HOST\',',
-  line   => 'define(\'DB_HOST\', \'127.0.0.1\');',
-  notify => Service['apache2'],
-}
+$sed_line = 's/define(\'DB_HOST\', \'localhost\');/define(\'DB_HOST\', \'127.0.0.1\');/'
 
-service { 'apache2':
-  ensure => running,
-  enable => true,
+exec { 'fix-wp-config-db-host':
+  command => "/bin/sed -i ${sed_line} /var/www/html/wp-config.php && service apache2 restart",
+  unless  => "/bin/grep -q \"define('DB_HOST', '127.0.0.1');\" /var/www/html/wp-config.php${''}",
+  path    => ['/bin', '/usr/bin'],
 }
 
