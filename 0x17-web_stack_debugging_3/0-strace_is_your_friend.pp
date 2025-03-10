ce0b15af71a8788804db6060e22acac93d105666
FRANKD-UX <frankdafrica@gmail.com>
2025-03-09 23:12:41 -0700

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,6 @@
-$sed_line = "s/define('DB_HOST', 'localhost');/define('DB_HOST', '127.0.0.1');/"
-
 exec { 'fix-wp-config-db-host':
   command => "/bin/sed -i \"s/define(\\'DB_HOST\\', \\'localhost\\');/define(\\'DB_HOST\\', \\'127.0.0.1\\');/\" /var/www/html/wp-config.php && service apache2 restart",
-  unless  => "/bin/grep -q \"define('DB_HOST', '127.0.0.1');\" /var/www/html/wp-config.php",
+  unless  => "[ -f /var/www/html/wp-config.php ] && /bin/grep -q \"define('DB_HOST', '127.0.0.1');\" /var/www/html/wp-config.php",
   path    => ['/bin', '/usr/bin'],
 }
 
