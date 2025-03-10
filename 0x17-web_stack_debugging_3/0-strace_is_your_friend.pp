ceb8a4663f7c06a3cb5ad41ea81f4504bc4f8667
FRANKD-UX <frankdafrica@gmail.com>
2025-03-09 23:42:47 -0700

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,22 @@
-# This Puppet manifest fixes the WordPress LAMP stack by updating the DB host in wp-config.php
-# It replaces 'localhost' with '127.0.0.1' to avoid Unix socket errors and restarts Apache.
+# This Puppet manifest updates WordPress DB host and restarts Apache to ensure proper connectivity.
 
-$sed_command = "/bin/sed -i 's/define(\\'DB_HOST\\', \\'localhost\\');/define(\\'DB_HOST\\', \\'127.0.0.1\\');/' /var/www/html/wp-config.php"
+class web_stack_fix {
+  
+  # Define the sed command to replace 'localhost' with '127.0.0.1' in wp-config.php
+  $sed_command = "/bin/sed -i 's/define(\\'DB_HOST\\', \\'localhost\\');/define(\\'DB_HOST\\', \\'127.0.0.1\\');/' /var/www/html/wp-config.php"
 
-exec { 'fix-wp-config-db-host':
-  command => "${sed_command} && service apache2 restart",
-  unless  => "/bin/grep -q \"define('DB_HOST', '127.0.0.1');\" /var/www/html/wp-config.php",
-  path    => ['/bin', '/usr/bin'],
-  require => File['/var/www/html/wp-config.php'],
+  exec { 'fix-wp-config-db-host':
+    command => "${sed_command} && service apache2 restart",
+    unless  => "/bin/grep -q \"define('DB_HOST', '127.0.0.1');\" /var/www/html/wp-config.php",
+    path    => ['/bin', '/usr/bin'],
+    require => File['/var/www/html/wp-config.php'],
+  }
+
+  file { '/var/www/html/wp-config.php':
+    ensure  => 'file',
+    require => Package['apache2'],
+  }
 }
 
-file { '/var/www/html/wp-config.php':
-  ensure  => 'file',
-  require => Package['apache2'],
-}
+include web_stack_fix
 
