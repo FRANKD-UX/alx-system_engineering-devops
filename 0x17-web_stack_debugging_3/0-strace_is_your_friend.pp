1a660472f422534af1dfa124351bae2c46b4a30e
FRANKD-UX <frankdafrica@gmail.com>
2025-03-10 13:28:03 +0000

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,8 @@
-# This Puppet manifest fixes the WordPress LAMP stack by updating the DB host in
-# wp-config.php from 'localhost' to '127.0.0.1' and restarting Apache.
+# a puppet code that fixes a wordpress site 5xx error to 200 ok
+# editing the mistyped .phpp to php in the /var/www/html/wp-settings.php file
 
-$sed_line = "\"s/define('DB_HOST', 'localhost');/define('DB_HOST', '127.0.0.1');/\""
-
-exec { 'fix-wp-config-db-host':
-  command => "/bin/sed -i ${sed_line} /var/www/html/wp-config.php && service apache2 restart",
-  unless  => "/bin/grep -q \"define('DB_HOST', '127.0.0.1');\" /var/www/html/wp-config.php",
-  path    => ['/bin', '/usr/bin'],
+exec { 'fix-wordpress-server-error':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/bin:/bin/',
 }
 
