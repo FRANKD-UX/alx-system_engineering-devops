cbb257ae91178eb34d39ec4ec194dc825f23722c
FRANKD-UX <frankdafrica@gmail.com>
2025-03-10 13:49:12 +0000

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This module contains a function that queries the Reddit API to get the number of subscribers for a given subreddit.
+"""
 import requests
 
 def number_of_subscribers(subreddit):
