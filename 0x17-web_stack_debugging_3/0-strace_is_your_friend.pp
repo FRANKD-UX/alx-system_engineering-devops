f7aace73643039394422f2a6fdb8b34c06ea0c1b
FRANKD-UX <frankdafrica@gmail.com>
2025-03-10 00:05:42 -0700

    y

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,11 @@
-# 0-strace_is_your_friend.pp
 # This Puppet manifest fixes the WordPress LAMP stack by updating the DB host in
 # wp-config.php from 'localhost' to '127.0.0.1' and restarting Apache.
 
-$sed_line = "s/define('DB_HOST', 'localhost');/define('DB_HOST', '127.0.0.1');/"
+$sed_line = "\"s/define('DB_HOST', 'localhost');/define('DB_HOST', '127.0.0.1');/\""
 
 exec { 'fix-wp-config-db-host':
-  command => "/bin/sed -i \"$sed_line\" /var/www/html/wp-config.php && service apache2 restart",
-  unless  => "/bin/grep -q 'define('DB_HOST', '127.0.0.1');' /var/www/html/wp-config.php",
+  command => "/bin/sed -i ${sed_line} /var/www/html/wp-config.php && service apache2 restart",
+  unless  => "/bin/grep -q \"define('DB_HOST', '127.0.0.1');\" /var/www/html/wp-config.php",
   path    => ['/bin', '/usr/bin'],
 }
 
