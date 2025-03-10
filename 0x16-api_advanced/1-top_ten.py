606b0f50c9efe47f454cd9b2f14cd9587fa54b75
FRANKD-UX <frankdafrica@gmail.com>
2025-03-10 14:47:55 +0000

    t

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains a function that queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+This module contains a function that queries
+ the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
 """
 import requests
 
