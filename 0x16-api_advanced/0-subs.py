ca63dd7597fc5e1f0f554e482d8fa7715bd5002f
FRANKD-UX <frankdafrica@gmail.com>
2025-03-10 14:32:14 +0000

    7

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,16 @@ This module contains a function that queries the Reddit API to get the number of
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = (
+        f"https://www.reddit.com/r/{subreddit}/about.json"
+    )
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
     else:
         return 0
-
