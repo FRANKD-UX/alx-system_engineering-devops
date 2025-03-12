040e01e754ee4586f9a2fb280ae0302096078e3b
FRANKD-UX <frankdafrica@gmail.com>
2025-03-12 11:29:21 +0000

    y

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,23 @@
-# Enable the user holberton to login and open files without error.
+class user_limit {
 
-# Increase hard file limit for Holberton user.
-exec { 'increase-hard-file-limit-for-holberton-user':
-    command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
-    path    => '/usr/local/bin/:/bin/'
+  # Ensure the correct limits exist for the holberton user
+  exec { 'increase-hard-file-limit-holberton-user':
+    command => 'sed -i "/holberton hard nofile/c\holberton hard nofile 50000" /etc/security/limits.conf || echo "holberton hard nofile 50000" >> /etc/security/limits.conf',
+    path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  }
+
+  exec { 'increase-soft-file-limit-holberton-user':
+    command => 'sed -i "/holberton soft nofile/c\holberton soft nofile 50000" /etc/security/limits.conf || echo "holberton soft nofile 50000" >> /etc/security/limits.conf',
+    path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  }
+
+  # Restart SSH to apply the new limits for new sessions
+  exec { 'restart-session-to-apply-limits':
+    command   => 'service ssh restart',
+    path      => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+    subscribe => [Exec['increase-hard-file-limit-holberton-user'], Exec['increase-soft-file-limit-holberton-user']],
+  }
 }
 
-# Increase soft file limit for Holberton user.
-exec { 'increase-soft-file-limit-for-holberton-user':
-    command => "sed -i '/^holberton soft/s/5/50000/' /etc/security/limits.conf",
-    path    => '/usr/local/bin/:/bin/'
-}
+include user_limit
 
