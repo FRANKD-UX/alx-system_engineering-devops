043ccd013d9959577d64959e2f1c25806c24b2be
FRANKD-UX <frankdafrica@gmail.com>
2025-03-12 11:55:23 +0000

    t

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,14 @@
 # Reconfigure the OS for 'holberton' to login and open a file without any error message
 
+# Increase hard file limit for the holberton user
 exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin:/bin'
+  command => 'sed -i "/holberton hard nofile/s/[0-9]\+/50000/" /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/bin']
 }
 
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin:/bin'
+# Increase soft file limit for the holberton user
+exec { 'increase-soft-file-limit-holberton-user':
+  command => 'sed -i "/holberton soft nofile/s/[0-9]\+/50000/" /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/bin']
 }
 
